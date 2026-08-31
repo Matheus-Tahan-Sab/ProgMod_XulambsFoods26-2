adea6d4caa45e747bff3b0fece8d94ff961454d1
Matheus Tahan Sab <sabmatheus24@gmail.com>
2026-08-31 11:54:00 -0300

    Incluido testes para pizza

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -1,20 +1,58 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PizzaTest {
 
+    Pizza pizza;
+
+    @BeforeEach
+    public void setUp(){
+        pizza = new Pizza();
+        pizza.adicionarIngredientes(2);
+    }
+
     @Test
     public void adicionaIngredientesCorretamente(){
-        //Arrange
-        Pizza pizza = new Pizza();
 
         //Act
-        int quantos = 
-            pizza.adicionarIngredientes(4);
+        int quantos = pizza.adicionarIngredientes(4);
 
         //Assert
-        assertEquals(4, quantos);
+        assertEquals(6, quantos);
     }
-    
+
+    @Test
+    public void naoAdicionaIngredienteNegativo(){
+
+        //Act
+        int quantos = pizza.adicionarIngredientes(-2);
+
+        //Assert
+        assertEquals(2,quantos);
+
+    }
+
+    @Test
+    public void naoUltrapassaMaximoDeIngredientes(){
+        
+        //Act
+        int quantos = pizza.adicionarIngredientes(7);
+
+        //Assert
+        assertEquals(2,quantos);
+
+    }
+
+    @Test
+    public void calcularPrecoCorreto(){
+
+       //Act 
+       double preco = pizza.valorFinal();
+
+       //Assert
+       assertEquals(39,preco,0.01);
+    }
+
 }
